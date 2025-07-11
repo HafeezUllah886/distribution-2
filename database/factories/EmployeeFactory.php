07175a2ae1611ff556e06db724df9618279ecb61
HafeezUllah886 <Hafeezullah886@gmail.com>
2025-07-12 01:42:42 +0500

    dsf

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class EmployeeFactory extends Factory
+class employeeFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
