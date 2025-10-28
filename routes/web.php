eff4b0c100222c8b512f61a6310f46615cfb85df
HafeezUllah886 <Hafeezullah886@gmail.com>
2025-10-28 14:26:41 +0500

    er

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use App\Models\accounts;
 use App\Models\products;
 use Illuminate\Support\Facades\Route;
 
-
 require __DIR__ . '/auth.php';
 require __DIR__ . '/finance.php';
 require __DIR__ . '/purchase.php';
@@ -32,20 +31,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [dashboardController::class, 'index'])->name('dashboard');
     Route::get('/admin/{branch?}/{from?}/{to?}', [adminDashboardController::class, 'index'])->name('admin.dashboard');
 
-
     Route::get('accounts_by_type/{type}', function ($type) {
         $accounts = accounts::where('type', $type)->currentBranch()->select('id as value', 'title as text')->get();
 
         return response()->json($accounts);
     })->name('accounts_by_type');
     
-
      Route::get('getSignleProduct/{id}', function ($id) {
         $product = products::with('units')->find($id);
 
         return $product;
     })->name('getSignleProduct');
-
 });
 
 
