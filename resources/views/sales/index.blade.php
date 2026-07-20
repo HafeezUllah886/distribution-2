75d44543089775439ed9649f895c80fb3e5ad1fd
HafeezUllah886 <Hafeezullah886@gmail.com>
2026-07-21 01:57:27 +0500

    done

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -75,7 +75,7 @@
                                     $paid = $sale->payments->sum('amount');
                                     $due = $amount - $paid;
                                 @endphp
-                                <tr class="{{ $sale->has_expense ? 'bg-danger' : '' }}">
+                                <tr class="{{ $sale->has_expense ? 'bg-info' : '' }}">
                                     <td>{{ $key + 1 }}</td>
                                     <td>{{ $sale->id }}</td>
                                     <td>{{ $sale->customer->title }}</td>
